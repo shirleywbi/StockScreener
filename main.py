50c05e805ea8a57ed352386246bce8b8b4edd27b
shirleywbi <swwbi@msn.com>
2020-08-06 22:13:02 -0700

    add red-white-blue back-testing results

--- main.py
+++ main.py
@@ -83,3 +83,65 @@ for i in df.index:
     num += 1
 
 print(percent_change_results)
+
+gains = 0
+num_gains = 0
+losses = 0
+num_losses = 0
+total_return = 1
+
+for result in percent_change_results:
+    if result > 0:
+        gains += result
+        num_gains += 1
+    else: 
+        losses += result
+        num_losses += 1
+    total_return = total_return * ((result/100) + 1)
+
+total_return = round((total_return - 1) * 100, 2)
+
+if (num_gains > 0):
+    avg_gain = gains/num_gains
+    max_return = str(max(percent_change_results))
+else:
+    avg_gain=0
+    max_return = "undefined"
+
+if (num_losses > 0):
+    avg_loss = losses/num_losses
+    max_loss = str(min(percent_change_results))
+    ratio = str(-avg_gain/avg_loss)
+else:
+    avg_loss=0
+    max_loss = "undefined"
+    ratio = "inf"
+
+if (num_gains > 0 or num_losses > 0):
+    batting_avg = num_gains/(num_gains + num_losses)
+else:
+    batting_avg = 0
+
+print("""
+    Results for {stock} going back to {date}, Sample size: {trade_count} trades
+    EMAs used: {emas}
+    Batting Avg: {batting_avg}
+    Gain/Loss ratio: {ratio}
+    Average Gain: {avg_gain}
+    Average Loss: {avg_loss}
+    Max Return: {max_r}
+    Max Loss: {max_l}
+    Total return over {trade_count} trades: {total_return}%
+""".format(
+    stock = stock, 
+    date = str(df.index[0]), 
+    trade_count = str(num_gains + num_losses), 
+    emas = str(emasUsed), 
+    batting_avg = batting_avg,
+    ratio = ratio,
+    avg_gain = avg_gain,
+    avg_loss = avg_loss,
+    max_r = max_return,
+    max_l = max_loss,
+    total_return = total_return
+))
